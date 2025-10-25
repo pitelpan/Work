3b79a8f2b34dd9f64167f8913c033b4c65976380
pitelpan <pitepana@gmail.com>
2025-10-25 22:54:29 +0300

    new

---
+++ statheres.c
@@ -0,0 +1,17 @@
+/* statheres.c: Programma pou deixnei tin xrisi statherwn */
+
+#include <stdio.h>
+
+#define N 100
+
+int main()
+{
+    int i, sum;
+    const int number = 10;
+
+    sum = 0;
+    for (i = number; i <= N; i++)
+        sum =sum + i;
+
+    printf("To athroisma twn arithmwn [%d..%d] einai %d", number, N, sum);
+}
