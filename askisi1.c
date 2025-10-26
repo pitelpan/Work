e412f2c19ca86466fbf383d988ca168a74596e95
pitelpan <pitepana@gmail.com>
2025-10-26 09:01:59 +0200

    add files

---
+++ askisi1.c
@@ -0,0 +1,15 @@
+/* askisi1.c */
+
+#include <stdio.h>
+
+int main()
+{
+    const int stathera = 100;
+    int metabliti;
+
+    printf("Dwse mia timi gia tin metabliti: ");
+    scanf("%d", &metabliti);
+    printf("H stathera einai: %d\n", stathera);
+    printf("H metabliti einai: %d\n", metabliti);
+    return 0;
+}
